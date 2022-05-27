1b629197f2ae9b0630224a6903ff678c8b0ddb55
Claire Louard <claire@MacBook-Air-de-Claire.local>
2022-05-27 19:19:08 +0200

    header enum

---
+++ src/enum.h
@@ -0,0 +1,19 @@
+//
+//  enum.h
+//  
+//
+//  Created by Claire Louard on 27/05/2022.
+//
+
+#ifndef enum_h
+#define enum_h
+
+enum class orientationPrincipale{N, E, S, O};
+
+enum class orientationPre{ON, NO, NE, EN, ES, SE, SO, OS};
+
+enum class Type{riviere, ville, abbaye, pre, route, jardin};
+
+enum class meeple_type{Normal, Abbe, Big};
+
+#endif /* enum_h */
