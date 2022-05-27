c0b7ff50e5f2ade83cff713c40e8a2539842eb64
Leo Peron <leo.peron@etu.utc.fr>
2022-05-27 15:21:12 +0200

    test

---
+++ src/gameengine.h
@@ -0,0 +1,22 @@
+//
+// Created by Leo Peron on 23/05/2022.
+//
+
+#ifndef PROJETCARCASSONELO21_GAMEENGINE_H
+#define PROJETCARCASSONELO21_GAMEENGINE_H
+
+#include "jeu.h"
+#include "plateau.h"
+
+class GameEngine {
+private:
+    Jeu jeu;
+    Plateau plateau;
+    bool chateau;
+    bool cathedrale;
+    bool jsp;
+public:
+    GameEngine(int nb_joueurs, bool ext1, bool ext2, bool ext3) :  ()
+};
+
+#endif //PROJETCARCASSONELO21_GAMEENGINE_H
---
+++ vuepartie.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Leo Peron on 23/05/2022.
+//
+
+#include "vuepartie.h"
