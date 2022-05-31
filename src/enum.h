8676408282e4219e34c7e76d9fc31a4896039c81
Leo Peron <leo.peron@etu.utc.fr>
2022-05-31 17:15:10 +0200

    Cardinaux updates

--- src/enum.h
+++ src/enum.h
@@ -4,9 +4,6 @@
 #include <list>
 #include <string>
 
-const std::list<std::string> orientationPrincipale = {"N", "E", "S", "O"};
-const std::list<std::string> orientationPre = { "ON", "NO", "NE", "EN", "ES", "SE", "SO", "OS"};
-
 class TypeCardinaux {
 public:
     enum points {nord, est, sud, ouest, ouest_nord, nord_ouest, nord_est, est_nord, est_sud, sud_est, sud_ouest, ouest_sud};
