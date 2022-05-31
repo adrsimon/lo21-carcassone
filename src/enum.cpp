a36f58dfa078d83fa6636196428bdfd776048e61
Leo Peron <leo.peron@etu.utc.fr>
2022-05-31 12:58:58 +0200

    Cardinaux updates

--- src/enum.cpp
+++ src/enum.cpp
@@ -10,14 +10,14 @@ TypeCardinaux::points TypeCardinaux::getOrientationInverse(points t) {
         case est: return ouest;
         case sud: return nord;
         case ouest: return est;
-        case nord_est: return ouest_sud;
-        case nord_ouest: return est_sud;
-        case sud_est: return ouest_nord;
-        case sud_ouest: return est_nord;
-        case est_sud: return nord_ouest;
-        case est_nord: return sud_ouest;
-        case ouest_nord: return sud_est;
-        case ouest_sud: return nord_ouest;
+        case nord_est: return sud_est;
+        case nord_ouest: return sud_ouest;
+        case sud_est: return nord_est;
+        case sud_ouest: return nord_ouest;
+        case est_sud: return ouest_sud;
+        case est_nord: return ouest_nord;
+        case ouest_nord: return est_nord;
+        case ouest_sud: return est_sud;
 
     }
 }
